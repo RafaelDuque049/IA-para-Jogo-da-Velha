e196514f4bad79bc1284f141c667072a6667f7da
Rafael_Duque <rafaellduque049@gmail.com>
2022-10-12 00:16:52 -0300

    Resetar a IA com um clique./reset the AI with one click
    adicionado a opção de apagar todo o progresso da IA de uma só vez./Added the option to erase all AI progress at once.

--- IA_jogo_da_Velha/artificial_intelligence/settings_infos/Info_IA.py
+++ IA_jogo_da_Velha/artificial_intelligence/settings_infos/Info_IA.py
@@ -1,7 +1,8 @@
 from time import sleep
 
 option = {
-    '1': 'Ver informações da IA(inforamções de Progresso)'
+    '1': 'Ver informações da IA(inforamções de Progresso)',
+    '2': 'Resetar a IA(Deletar progresso)'
 }
 link_direct = {
     'random': 'IA_jogo_da_Velha/artificial_intelligence/moves/random.txt',
@@ -11,7 +12,8 @@ link_direct = {
 
 print('\nO que deseja fazer?\n\n'\
     f'1.{option.get("1")}\n'\
-    f'2.Calcelar ações\n')
+    f'2.{option.get("2")}\n'\
+    f'3.Calcelar ações\n')
 action = str(input("escolha sua ação: "))
 
 
@@ -30,4 +32,29 @@ if action == '1' or action == '':
 
 
 elif action == '2':
+    sleep(.5)
+    lines_random, lines_standard = \
+        len((open(link_direct.get("random"), 'r')).readlines()),\
+        len((open(link_direct.get("standard"), 'r')).readlines())
+    
+    confirm = str(input(f'\n{"=-"*40}\nserá deletado ao todo {lines_random+lines_standard} '
+        'informações(progresso) da IA. deseja mesmo prosseguir? [S/N]: '))
+
+    if confirm.upper() == 'S':
+        sleep(.7)
+        
+        try:
+            del_random = (open(link_direct.get("random"), 'w')).close()
+            del_standard = (open(link_direct.get("standard"), 'w')).close()
+            del_next_update = (open(link_direct.get("updates_moves"), 'w')).close()
+
+        finally:
+            print(f'\ninformações de IA deletada com sucesso.\n{"=-"*40}')
+
+    else:
+        print(f'\nfoi cancelado qualquer tipo de ação de deletar informações.'\
+            f'nenhuma informação foi deletada.\n{"=-"*40}\n')
+
+
+elif action == '3':
     print('\nfoi cancelado qualquer tipo de ação.\n')
